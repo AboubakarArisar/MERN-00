d7305ca3a156282075f22b5d72c73eaaf51117a4
AboubakarArisar <aboubakar.arisarsahab@gmail.com>
2023-11-01 14:56:55 +0500

    Day 3 week 2 Tasks

---
+++ Week2Day3/index.js
@@ -0,0 +1,21 @@
+const firstPerson = {
+    name : "Abdullah",
+    address : "TandoAdam"
+};
+
+const secondPerson = {
+    name : "Hashir",
+    address : "TandoAdam"
+};
+
+const thirdPerson = {
+    name : "Aboubakar",
+    address : "Saddau"
+};
+
+
+const array = [firstPerson,secondPerson,thirdPerson];
+for(let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
+
