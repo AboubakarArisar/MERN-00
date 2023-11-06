8f5d34b0f2b9e68a5f0cec5cf552219a53c423e6
AboubakarArisar <aboubakar.arisarsahab@gmail.com>
2023-11-07 00:43:07 +0500

    designing todo app done

--- AssignmentJs/Task/src/Todo.jsx
+++ AssignmentJs/Task/src/Todo.jsx
@@ -1,42 +1,86 @@
 import { useState } from "react";
-import './App.css';
+import "./App.css";
 
 function Todo() {
-    const [todos, setTodo] = useState([]);
-    const [task, setTask] = useState("");
-    const [status, setStatus] = useState("Incomplete");
+  const [todos, setTodo] = useState([]);
+  const [task, setTask] = useState("");
+  const [status, setStatus] = useState("Incomplete");
+  const [date, setDate] = useState(new Date());
 
-    return (
-        <>
-        <div className="container">
-            <div className="form">
-            <form onSubmit={(event) => {
-                event.preventDefault();
-            }}>
-                <input required type="text" value={task} onChange={(event)=>{
-                    setTask(event.target.value);
-                }} />
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (task.trim() === "") {
+      alert("Task name is required.");
+    } else {
+      const newTodo = { task: task, status: status, date: date };
+      setTodo([...todos, newTodo]);
+      setTask("");
+      setDate(new Date());
+    }
+  };
 
-                <select value={status} onChange={(event)=>{
-                    setStatus(event.target.value);
-                }}>
-                    <option value="Incomplete">Incomplete</option>
-                    <option value="complete">Complete</option>
-                </select>
-                <button type="submit" onClick={() => {
-                    setTodo([...todos, { task: task, status: status }]);
-                }}>Submit</button>
-            </form>
-            </div>
-            <div className="list">
-                {todos.map((item, index) => (
-                    <div key={index}>
-                        <span>{item.task+"   " }{ item.status}</span>
-                    </div>
-                ))}
-            </div>
+  const formatTime = (time) => {
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes < 10 ? "0" + minutes : minutes;
+    const formattedSeconds = seconds < 10 ? "0" + seconds : seconds;
+    return `${formattedHours}:${formattedMinutes}:${formattedSeconds} ${ampm}`;
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="todo-heading">TODO Application</h1>
+        <div className="form">
+          <form onSubmit={handleSubmit}>
+            <input
+              required
+              placeholder="Task name"
+              type="text"
+              value={task}
+              onChange={(event) => {
+                setTask(event.target.value);
+              }}
+            />
+
+            <select
+              value={status}
+              onChange={(event) => {
+                setStatus(event.target.value);
+              }}
+            >
+              <option value="Incomplete">Incomplete</option>
+              <option value="complete">Complete</option>
+            </select>
+            <button type="submit">Submit</button>
+          </form>
         </div>
-        </>
-    )
+        <div className="list">
+          <table className="todo-table">
+            <thead>
+              <tr>
+                <th>Task</th>
+                <th>Status</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {todos.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.task}</td>
+                  <td>{item.status}</td>
+                  <td>{formatTime(item.date)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
 }
+
 export default Todo;
