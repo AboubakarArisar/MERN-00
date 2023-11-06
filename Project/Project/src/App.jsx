6df13cc79ec8328fb8b5e3142cca382881dd0f83
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2023-11-06 14:08:55 +0500

    login Page done

---
+++ Project/Project/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import './App.css'
+function Login() {
+  return (
+    <>
+    <div className="container">
+      <div className="box">
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="********" />
+        <button>Login</button>
+      </div>
+    </div>
+    </>
+  )
+}
+function App() {
+
+
+  return (
+    <>
+     <Login />
+    </>
+  )
+}
+
+export default App
