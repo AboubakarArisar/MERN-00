78b80b0c13c5dd9306339b8ae47af2571bad8b9d
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2023-11-06 18:23:06 +0500

    Assignment done

--- AssignmentJs/Task/src/App.jsx
+++ AssignmentJs/Task/src/App.jsx
@@ -1,51 +1,44 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
   const [friendName, setFriendName] = useState("");
   const [friendsList, setFriendsList] = useState([]);
 
-  const handleNameChange = (event) => {
-    setFriendName(event.target.value);
-  };
-
-  const handleAddFriend = () => {
-    if (friendName.trim() !== "") {
-      setFriendsList([...friendsList, friendName]);
-      setFriendName("");
-    }
-  };
-
-  const handleClearText = () => {
-    setFriendName("");
-  };
-
-
   return (
-    <>
-      <div className="container">
-        <h1>Assignment Question no 6</h1>
-        
-        <div className="box">
-          <input type="text"
-           placeholder="Friend name"  
-           value={friendName}
-           onChange={handleNameChange}/>
-          <button onClick={handleClearText}>Clear</button>
-          <button onClick={handleAddFriend}>Save</button>
-          
-        </div>
-        <div className="list">
-        <ul>
-        {friendsList.map((friend, index) => (
-          <li key={index}>{friend}</li>
-        ))}
-      </ul>
-        </div>
+    <div className="container">
+      <h1>Assignment Question no 6</h1>
+
+      <div className="box">
+        <form
+          onSubmit={function (event) {
+            event.preventDefault();
+            setFriendsList([...friendsList, friendName]);
+            setFriendName("");
+          }}
+        >
+          <input
+            type="text"
+            placeholder='friend name'
+            value={friendName}
+            onChange={function (event) {
+              setFriendName(event.target.value);
+            }}
+          />
+          <input id='button' type="submit" value="Save" />
+        </form>
+   
       </div>
-    </>
-  )
+      <div className='list'>
+        <h2>Friends list</h2>
+          <ul>
+            {friendsList.map((friend, index) => (
+              <li key={index}>{friend}</li>
+            ))}
+          </ul>
+        </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
